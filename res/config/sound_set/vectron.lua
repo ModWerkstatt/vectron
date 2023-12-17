55ab92d7b716e071522bf98bf2541f3501dc66e3
Florian Thiel <flth@flth.de>
2023-12-17 20:46:01 +0100

    inital von Dylan

---
+++ res/config/sound_set/vectron.lua
@@ -0,0 +1,75 @@
+local soundsetutil = require "soundsetutil"
+
+function data()
+
+local data = soundsetutil.makeSoundSet()
+		
+soundsetutil.addTrackParam01
+(data, "vehicle/train/br193/idle.wav", 25.0,
+		{
+			{ 0.0, 1.0 },
+			{ 0.01, 1.0 },
+			{ 0.1, 0.0 } },
+		{
+			{ 0.0, 1.0 },
+			{ 1.0, 1.0 } }, 
+		"speed01")
+
+		
+soundsetutil.addTrackParam01
+(data, "vehicle/train/br193/summ.wav", 25.0,
+		{
+			{ 0.00, 0.0 },
+			{ 0.01, 0.0},
+			{ 0.05, 1.0},
+			{ 0.10, 0.0},
+			{ 1.00, 0.0},
+			
+		},
+		{
+			{ 0.0, 0.9 }, 
+			{ 1.0, 1.0 } 
+		}, 
+		"speed01")	
+		
+		
+soundsetutil.addTrackParam01
+(data, "vehicle/train/br193/fahren.wav", 25.0,
+		{
+			{ 0.00, 0.0},
+			{ 0.01, 0.0},
+			{ 0.03, 0.3},
+			{ 0.2, 0.7},
+			{ 0.5, 0.9},
+			{ 0.51, 0.0},
+			
+		},
+		{
+			{ 0.0, 0.6 }, 
+			{ 1.0, 1.0 } 
+		}, 
+		"speed01")
+
+soundsetutil.addTrackParam01
+(data, "vehicle/train/vectron/fahren.wav", 25.0,
+        {
+            { 0.50, 0.0},
+            { 0.51, 1.0},
+            { 1.00, 1.0},
+
+        },
+        {
+            { 0.0, 0.0 }, 
+            { 0.5, 1.0 },
+            { 1.0, 1.2 } 
+        }, 
+        "speed01")
+		
+soundsetutil.addEvent(data, "horn", { "vehicle/train/br193/horn.wav" }, 25.0)
+
+soundsetutil.addTrackSqueal(data, "vehicle/train/br193/kurve.wav", 25.0)
+soundsetutil.addTrackBrake(data, "vehicle/train/br193/bremsen.wav", 25.0, 1.0)
+
+return data
+
+end
---
+++ strings.lua
@@ -0,0 +1,317 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Siemens Vectron MS and AC",
+		["mod_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. The Vectron can operate in 20 European countries. The Vectron was first presented to the public at the Innotrans in Berlin in 2010, and since then the locomotive has been in use on many state and private railways. The package contains the locomotives br193 MS (multi-system), br193 AC (alternating current) and the Smartron.",
+
+        ["br193_ms_menu"] = "Vectron Class 193 MS",
+        ["br193_ac_menu"] = "Vectron Class 193 AC",
+        ["br193_fake_ms_menu"] = "Vectron Class 193 MS FAKES",
+        ["br193_fake_ac_menu"] = "Vectron Class 193 AC FAKES",
+		["br193_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. In 2014 the Vectron MS was presented for the first time with a speed of 200km/h (also available with 160km/h) and a continuous output of 6400kW.",
+		
+		["br192_menu"] = "Smartron Class 192",
+		["br192_fake_menu"] = "Smartron Class 192",
+		["br192_desc"] = "The Smartron is a slimmed down version of the Vectron. The Smartron is only available in Blue Metallic, the power is 5600kW and is only approved for 140km/h, the locomotive only runs in one country at a time. Smartron has the train protection systems PZB/LZB and ETCS for Germany.",
+				
+		["br193_ms"] = 	"Vectron as a multi-system locomotive with 4 pantographs",
+		["br193_ac"] = 	"Vectron as a multi-system locomotive with 2 pantographs",
+		["br193_zza"] = "Vectron as an AC locomotive with 2 pantographs and a train destination indicator",
+		["br192"] = 	"Smartron as AC locomotive with 2 pantographs",
+				
+		["br193_ms_siemens_name"] = 	"Class 193 901 MS Siemens test loco",
+		["br193_ms_sbb_rot_name"] = 	"Class 193 471 MS SBB Cargo International",
+		["br193_ms_sbb_donau_name"] = 	"Class 193 521 MS SBB Cargo International",
+		["br193_ms_sbb_holland_name"] = "Class 193 525 MS SBB Cargo International",
+		["br193_ms_sbb_cenerie_name"] = "Class 193 518 MS SBB Cargo International",
+		["br193_ms_sbb_night_name"] = 	"Class 193 532 MS SBB Cargo International",
+		["br193_ms_sbb_schwarz_name"] = "Class 193 658 MS SBB Cargo International",
+		["br193_ms_sbb_ruhr_name"] = 	"Class 193 701 MS SBB Cargo International",
+		["br193_ms_sbb_silver_name"] = 	"Class 6193 107 MS SBB Cargo International",
+		["br193_ms_sbb_ell_name"] = 	"Class 193 258 MS SBB Cargo International",
+		["br193_ms_ell_igs_name"] = 	"Class 193 214 MS SBB Cargo International",
+		["br193_ms_db_name"] = 			"Class 193 302 MS DB Cargo",
+		["br193_ms_db2_name"] = 		"Class 193 310 MS DB Cargo",
+		["br193_ms_db3_name"] = 		"Class 193 338 MS DB Cargo",
+		["br193_ms_db4_name"] = 		"Class 193 410 MS DB Cargo",
+		["br193_ms_db5_name"] = 		"Class 193 560 MS DB Cargo",
+		["br193_ms_mrce_name"] = 		"Class 193 624 MS MRCE",
+		["br193_ms_mrce_bls_name"] = 	"Class 193 712 MS MRCE/BLS",
+		["br193_ms_hupac_name"] = 		"Class 193 492 MS Hupac",
+		["br193_ms_hupac_bls_name"] = 	"Class 193 465 MS Hupac BLS",
+		["br193_ms_wrs_name"] = 		"Class 193 475 MS WRS",
+		["br193_ms_rpool_name"] = 		"Class 193 093 MS Railpool",
+		["br193_ms_ell_750_name"] = 	"Class 193 756 MS ELL Vectron #750",
+		["br193_ms_ell_name"] = 		"Class 193 299 MS ELL",
+		["br193_ms_ell_ns_name"] = 		"Class 193 766 MS ELL",
+		["br193_ms_cap_name"] = 		"Class 193 785 MS Captrain",
+		["br193_ms_boxx_name"] = 		"Class 193 833 MS BoxXpress",
+		["br193_ms_rail_name"] = 		"Class 193 623 MS Rail Force One",
+		["br193_ms_metrans_name"] = 	"Class 383 401 MS Metrans",
+		["br193_ms_akiem_name"] = 		"Class 193 885 MS Akiem",
+		["br193_ms_setg_name"] = 		"Class 193 839 MS SETG alpine locomotive",
+		["br193_ms_setg_fried_name"] =  "Class 193 691 MS SETG peace engine",
+		["br193_ms_setg_jeder_name"] = 	"Class 193 746 MS SETG Anyone",
+		["br193_ms_setg_polo_name"] = 	"Class 193 736 MS SETG Markopolo",
+		["br193_ms_setg_polo_name"] = 	"Class 193 692 MS SETG Woodworks",
+		["br193_ms_ell_regiojet_name"] = "Class 193 206 MS regional jet",		
+		["br193_ms_ell_regiower_name"] = "Class 193 227 MS regional jet Advertising",
+		
+		["br193_ms_bls_name"] = 		"Class 475 405 MS BLS alpinists",
+				
+		["br193_ms_öbb_rcc_name"] = 	"Class 1293 012 MS ÖBB",
+		["br193_ms_öbb_name"] = 		"Class 1293 015 MS ÖBB",
+		["br193_ms_öbb_500_name"] = 	"Class 1293 018 MS ÖBB",
+		["br193_ms_öbb_logo_name"] = 	"Class 1293 069 MS ÖBB",
+		["br193_ms_öbb_bel_name"] = 	"Class 1293 175 MS ÖBB",
+		
+		["br193_ac_siemens_name"] = 	"Class 193 930 AC Siemens test loco",
+		["br193_ac_sbb_ell_name"] = 	"Class 193 209 AC SBB Cargo International",
+		["br193_ac_ell_name"] = 		"Class 193 208 AC ELL",
+		["br193_ac_ell_weiß_name"] = 	"Class 193 247 AC ELL",
+		["br193_ac_ell_flix_name"] = 	"Class 193 231 AC ELL",
+		["br193_ac_rpool_name"] = 		"Class 193 990 AC Railpool",
+		["br193_ac_rpool_zza_name"] = 	"Class 193 201 AC Railpool Bavaria",
+		["br193_ac_rpool_weiß_name"] = 	"Class 193 813 AC Railpool white",
+		["br193_ac_rpool_flix_name"] = 	"Class 193 813 AC Railpool",
+		["br193_ac_rpool_ohne_name"] = 	"Class 193 803 AC Railpool",
+		["br193_ac_north_name"] = 		"Class 193 921 AC Northrail",
+		["br193_ac_cap_name"] = 		"Class 193 894 AC Captrain",
+		["br193_ac_mrce_name"] = 		"Class 193 247 AC MRCE",
+		["br193_ac_mrce_flix_name"] = 	"Class 193 862 AC MRCE",
+		["br193_ac_mrce_boxx_name"] = 	"Class 193 852 AC MRCE",
+		["br193_ac_mrce_zza_name"] = 	"Class 193 247 AC MRCE Zza",
+		["br193_ac_boxx_name"] = 		"Class 193 880 AC BoxXpress",
+		["br193_ac_setg_15_name"] = 	"Class 193 218 AC SETG Bethoven 15 Years",
+		["br193_ac_setg_20_name"] = 	"Class 193 218 AC SETG Bethoven 20 Years",
+		["br193_ac_setg_name"] = 		"Class 193 240 AC SETG",
+		["br193_ac_setg_doppler_name"] = "Class 193 813 AC SETG Christian Doppler",
+		["br193_ac_setg_wolfgangname"] = "Class 193 204 AC SETG Wolfgang Amadeus",
+		["br193_ac_setg_nacht_name"] = 	"Class 193 219 AC SETG silent Night",
+		
+		
+		["br193_ac_care_name"] = 		"Class 476 451 AC RailCare",		
+		
+		["br192_siemens_blau_name"] = 	"Class 192 001 Siemens",
+		["br192_blau_name"] = 			"Class 192 002",
+		["br192_nrail_name"] = 			"Class 192 017 Northrail",
+		["br192_rhc_name"] = 			"Class 192 055 Rhein Cargo",
+		["br192_txl_blau_name"] = 		"Class 192 010 TXL Blue Wonder",
+		["br192_txl_natur_name"] = 		"Class 192 011 TXL natural reserve",
+		["br192_nrail_flex_name"] = 	"Class 192 059 Northrail Flex Bahndienstleistungen",
+
+        -- Vorspann
+        ["Vorspann_br193"] = "Active lead engines?",
+        ["option_vorspann_br193_desc"] = "Active the lead engines (with front panto rised)?",
+		
+        ["br193_vorspann_ob_name"] = "Lead - Class 193",
+
+        -- Fake
+        ["Fake_br193"] = "Active fake engines?",
+        ["option_fake_br193_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
+		
+        ["br193_fake_siemens_name"] = "Fake - Class 193 Siemens",
+
+	},
+	de = {
+
+		["mod_name"] = "Siemens Vectron MS und AC",
+		["mod_desc"] = "Der Vectron ist eine moderene Drehstromlokomotive für den Internationalen Personen- und Güterverkehr in Europa. Der Vectron kann in 20 Europäischen Ländern verkehren. Erstmal wurde der Vectron im Juli 2010 in dem  Prüfcenter Wegberg-Wildenrath der öffentlichkeit vorgestellt seid dem ist die Lok bei vielen Staats- und Privatbahnen im Einsatz. In dem Paket sind die Loks br193 MS(Mehrsystem), br193 AC(Wechselstrom) und der Smartron enthalten. ",
+
+        ["br193_ms_menu"] = "Vectron BR 193 MS", 
+		["br193_ac_menu"] = "Vectron BR 193 AC",  
+		["br193_dc_menu"] = "Vectron BR 193 DC",
+        ["br193_fake_ms_menu"] = "Vectron BR 193 MS Fakes",
+        ["br193_fake_ac_menu"] = "Vectron BR 193 AC Fakes",
+        ["br193_fake_dc_menu"] = "Vectron BR 193 DC Fakes",
+		["br193_desc"] = "Der Vectron ist eine moderene Drehstromlokomotive für den Internationalen Personen- und Güterverkehr in Europa. 2014 wurde erstmal der Vectron MS vorgestellt mit einer Geschwindigkeit von 200km/h(auch mit 160km/h erhältlich) und einer Dauerleistung von 6400kW.",
+		
+		["br192_menu"] = "Smartron BR 192",
+		["br192_fake_menu"] = "Smartron BR 192",
+		["br192_desc"] = "Die Smartron ist eine abgespeckte Version der Vectron. Die Smartron ist nur in Blau Metalic erhältlich, die Leistung beträgt 5600kW und ist nur auf 140km/h zugelassen, die Lok auf immer nur in einem Land verkehren. Die Smartron besitz für Deutschland die Zugsicherungssysteme PZB/LZB und ETCS. ",
+				
+		["br193_ms"] = 	"Vectron als mehrsystem Lok mit 4 Stromabnehmer",
+		["br193_ac"] = 	"Vectron als Wechselstrom Lok mit 2 Stromabnehmer",
+		["br193_zza"] = "Vectron als Wechselstrom Lok mit 2 Stromabnehmer und Zugzielanzeiger",
+		["br192"] = 	"Smartron als Wechselstrom Lok mit 2 Stromabnehmer",
+			
+			
+		["br193_ms_siemens_name"] = 	"BR 193 901 MS Siemens",
+		["br193_ms_siemens2_name"] = 	"BR 6193 400 MS Siemens (Vectron 230km/h)",
+		["br193_ms_siemens3_name"] = 	"BR 193 844 MS Siemens (Creating Corridors)",
+				
+		["br193_ms_akiem_name"] = 		"BR 193 888 MS Akiem (ohne Italien)",
+		["br193_ms_akiem2_name"] = 		"BR 193 890 MS Akiem (Ecco Rail)",
+		
+		["br193_ms_alpha_name"] = 		"BR 193 403 MS Alpha Trains (mit Italien)",
+		["br193_ms_alpha2_name"] = 		"BR 193 588 MS Alpha Trains (ohne Italien)",
+		["br193_ms_alpha3_name"] = 		"BR 193 586 MS Alpha Trains (CD Cargo)",
+		["br193_ms_alpha4_name"] = 		"BR 193 405 MS Alpha Trains (Green Cargo)",
+		["br193_ms_alpha5_name"] = 		"BR 193 587 MS Alpha Trains (CD Cargo)",
+		
+		["br193_ms_bls_name"] = 		"BR 193 494 MS BLS Cargo (Alpinisten)",	
+		["br193_ms_bls2_name"] = 		"BR 475 402 MS BLS Cargo (Alpinisten)",	
+		["br193_ms_bls3_name"] = 		"BR 475 422 MS BLS Cargo (The New Alpinist",	
+		["br193_ms_bls4_name"] = 		"BR 475 416 MS BLS Cargo (ohne Folierung)",	
+		
+		["br193_ms_boxx_name"] = 		"BR 193 833 MS BoxXpress.de",	
+		["br193_ms_boxx2_name"] = 		"BR 6193 117 MS BoxXpress.eu",	
+		
+		["br193_ms_captrain_name"] = 	"BR 193 892 MS Captrain (Jerome)",
+		
+		["br193_ms_cu_name"] = 			"BR 370 052 MS Cargounit",	
+		["br193_ms_cu2_name"] = 		"BR 370 051 MS Cargounit/CD Cargo",	
+		
+		["br193_ms_db_name"] = 			"BR 193 302 MS DB (mit Italien)",
+		["br193_ms_db2_name"] = 		"BR 193 394 MS DB (Schienenbrücke Ukraine)",	
+		["br193_ms_db3_name"] = 		"BR 193 371 MS DB (ohne Italien)",
+		["br193_ms_db4_name"] = 		"BR 193 388 MS DB (#DBCargofährt)",	
+		["br193_ms_db5_name"] = 		"BR 193 560 MS DB (Güter gehören auf die Schiene)",	
+		["br193_ms_db6_name"] = 		"BR 193 360 MS DB (I am European)",
+		["br193_ms_db7_name"] = 		"BR 193 363 MS DB (I am a Climate Hero)",
+		["br193_ms_db8_name"] = 		"BR 193 312 MS DB (Das ist Grün Offenburg)",
+		["br193_ms_db9_name"] = 		"BR 193 357 MS DB (Das ist Grün Saarbrücken)",
+		["br193_ms_db10_name"] = 		"BR 193 361 MS DB (Das ist Grün Senftenberg)",
+		
+		["br193_ms_hupac_name"] = 		"BR 193 492 MS Hupac",	
+		["br193_ms_hupac2_name"] = 		"BR 193 493 MS Hupac/WRS",	
+		
+		["br193_ms_ell_name"] = 		"BR 193 214 MS ELL (ohne Italien)",
+		["br193_ms_ell2_name"] = 		"BR 193 299 MS ELL (mit Italien)",		
+		["br193_ms_ell3_name"] = 		"BR 193 756 MS ELL (Vectron #750)",
+		["br193_ms_ell4_name"] = 		"BR 193 759 MS ELL (NS Niederlande)",
+		["br193_ms_ell5_name"] = 		"BR 193 692 MS ELL (Nightjet)",
+		["br193_ms_ell6_name"] = 		"BR 193 293 MS ELL (Ceske Drahy)",
+		["br193_ms_ell7_name"] = 		"BR 383 003 MS CD Cargo",
+		
+		["br193_ms_lomo_name"] = 		"BR 193 772 MS Lokomotion (Steifen Blau)",
+		["br193_ms_lomo2_name"] = 		"BR 193 777 MS Lokomotion (Beige)",
+		["br193_ms_lomo3_name"] = 		"BR 193 776 MS Lokomotion (Steifen Rot)",	
+		["br193_ms_lomo4_name"] = 		"BR 193 774 MS Lokomotion (20 Years)",
+		["br193_ms_lomo5_name"] = 		"BR 193 664 MS Lokomotion (MRCE)",
+		
+		["br193_ms_mrce_name"] = 		"BR 193 661 MS MRCE",
+		["br193_ms_mrce2_name"] = 		"BR 193 639 MS BEACON",
+		["br193_ms_mrce3_name"] = 		"BR 193 706 MS MRCE (Mercitalia)",
+		["br193_ms_mrce4_name"] = 		"BR 193 639 MS BEACON (Mercitalia)",
+		
+		["br193_ms_obb_name"] = 		"Rh 1293 001 MS ÖBB",
+		
+		["br193_ms_rpool_name"] = 		"BR 6193 093 MS Railpool (ohne Italien)",	
+		["br193_ms_rpool2_name"] = 		"BR 6193 112 MS Railpool (mit Italien)",	
+	
+		["br193_ms_pkp_name"] = 		"BR 370 056 MS PKP Cargo (Dach Neongrün)",
+		["br193_ms_pkp2_name"] = 		"BR 370 027 MS PKP Cargo (Dach Rot)",
+		["br193_ms_pkp3_name"] = 		"BR 370 022 MS PKP Cargo (Dach Blau)",
+		["br193_ms_pkp4_name"] = 		"BR 383 053 MS PKP Cargo (Unitcredit) ",
+		
+		["br193_ms_sbb_name"] = 		"BR 6193 107 MS SBBCINT Silverpiercer (Bodensee)",
+		["br193_ms_sbb2_name"] = 		"BR 193 258 MS SBBCINT/ELL",
+		["br193_ms_sbb3_name"] = 		"BR 193 658 MS SBBCINT Shadowpiercer (Vierwaldstättersee)",
+		["br193_ms_sbb4_name"] = 		"BR 193 469 MS SBBCINT Alppiercer (Simplon)",
+		["br193_ms_sbb5_name"] = 		"BR 193 516 MS SBBCINT Alpiercer 2 (Aare)",
+		["br193_ms_sbb6_name"] = 		"BR 193 518 MS SBBCINT Monte Cenerie (Ticino)",
+		["br193_ms_sbb7_name"] = 		"BR 193 518 MS SBBCINT Monte Cenerie (5 Jahre Rastatt)",		
+		["br193_ms_sbb8_name"] = 		"BR 193 525 MS SBBCINT Hollandpiercer (Rotterdam)",
+		["br193_ms_sbb9_name"] = 		"BR 193 701 MS SBBCINT Ruhrpiercer (Ruhrgebiet)",
+		
+		["br193_ms_sbb10_name"] = 		"BR 193 532 MS SBBCINT Nightpiercer (Rhein)",
+		["br193_ms_sbb11_name"] = 		"BR 6193 110 MS SBBCINT Goldpiercer (Zuckersee)",
+		["br193_ms_sbb12_name"] = 		"BR 193 659 MS SBBCINT Güterbahnen (Berlin)",
+		
+		["br193_ms_setg_name"] = 		"BR 193 839 MS SETG (Alpenlok)",
+		["br193_ms_setg2_name"] = 		"BR 193 821 MS SETG (Daniel)",
+		["br193_ms_setg3_name"] = 		"BR 193 746 MS SETG (Jeder Mann)",
+		["br193_ms_setg4_name"] = 		"BR 193 691 MS SETG (Die Waffen nieder)",
+		["br193_ms_setg5_name"] = 		"BR 193 736 MS SETG (Marco Polo)",
+		["br193_ms_setg6_name"] = 		"BR 193 692 MS SETG (Wood Works)",
+	
+		--- AC ---
+	
+		["br193_ac_siemens_name"] = 	"BR 192 961 AC Siemens",
+		["br193_ac_siemens2_name"] = 	"BR 193 780 AC Siemens Vectron 230km/h",
+		
+		["br193_ac_dsb_name"] = 		"EB 3202 AC DSB",
+		["br193_ac_dsb2_name"] = 		"EB 3240 AC DSB Vectron Nr. 1000",
+		
+		["br193_ac_ell_name"] = 		"BR 193 208 AC ELL",
+		["br193_ac_ell2_name"] = 		"BR 193 208 AC ELL/Rurtalbahn Cargo",
+		["br193_ac_ell3_name"] = 		"BR 193 208 AC ELL/Eccorail",
+		["br193_ac_ell4_name"] = 		"BR 193 247 AC ELL (Farbe Weiß)",
+	
+		["br193_ac_flix_name"] = 		"BR 243 002 AC Flixtrian/Hectorrail",
+		["br193_ac_flix2_name"] = 		"BR 193 813 AC Flixtrian",
+		["br193_ac_flix3_name"] = 		"BR 193 861 AC Flixtrian",
+				
+		["br193_ac_mrce_name"] = 		"BR 193 874 AC MRCE",
+		["br193_ac_mrce2_name"] = 		"BR 193 603 AC MRCE",
+		["br193_ac_mrce3_name"] = 		"BR 193 852 AC MRCE",
+		["br193_ac_mrce4_name"] = 		"BR 193 864 AC MRCE",
+		
+		["br193_ac_nrail_name"] = 		"BR 193 922 AC Northrail",
+		
+		["br193_ac_retrack_name"] = 	"BR 193 815 AC Retrack (Kätchen)",
+		["br193_ac_retrack2_name"] = 	"BR 193 811 AC Retrack (Jule)",
+		["br193_ac_retrack3_name"] = 	"BR 193 872 AC Retrack (Transpetrol)",
+		
+		["br193_ac_rpool_name"] = 		"BR 193 802 AC Railpool Regio Bayern",
+		["br193_ac_rpool2_name"] = 		"BR 193 994 AC Railpool",		
+		["br193_ac_rpool3_name"] = 		"BR 193 813 AC Railpool",				
+		["br193_ac_rpool4_name"] = 		"BR 193 813 AC Railpool (Knorrbremse)",	
+		["br193_ac_rpool5_name"] = 		"BR 193 804 AC Railpool",	
+
+		["br193_ac_sbb_name"] = 		"BR 193 210 AC SBBCINT/ELL",	
+
+		["br193_ac_setg_name"] = 		"BR 193 218 AC SETG (Beethoven 15 Jahre SETG)",		
+		["br193_ac_setg2_name"] = 		"BR 193 218 AC SETG (Beethoven 20 Jahre SETG)",			
+		["br193_ac_setg3_name"] = 		"BR 193 218 AC SETG (Stille Nacht Heilige Nacht)",			
+		["br193_ac_setg4_name"] = 		"BR 193 812 AC SETG (Weiß)",				
+		["br193_ac_setg5_name"] = 		"BR 193 204 AC SETG (Mozart)",				
+		["br193_ac_setg6_name"] = 		"BR 193 831 AC SETG (Christian Doppler)",				
+			
+		--- Smartron ---	
+		
+		["br192_sm_siemens_name"] = 	"BR 192 001 Siemens",
+		
+		["br192_sm_bbl_name"] = 		"BR 192 008 BBL Logistik",
+			
+		["br192_sm_evb_name"] = 		"BR 192 005 EVB",
+		["br192_sm_evb2_name"] = 		"BR 192 076 EVB (Black Forest Terminal)",
+		
+		["br192_sm_nrail_name"] = 		"BR 192 017 Northrail",
+		["br192_sm_nrail2_name"] = 		"BR 192 015 Northrail/ RTB Cargo",
+					
+		["br192_sm_rhc_name"] = 		"BR 192 045 Rhein Cargo",
+		
+		["br192_sm_pimk_name"] = 		"BR 192 962 Pimk Rail",
+				
+		--- DC ---
+	
+		["br193_dc_siemens_name"] = 	"BR 170 020 Siemens",
+		
+		["br193_dc_db_name"] = 			"BR 170 021 DB Cargo (Polen)",
+		
+		["br193_dc_db2_name"] = 		"BR 191 013 DB Schenker (Italien)",
+		
+		["br193_dc_mrce_name"] = 		"BR 191 022 MRCE (Italien)",
+				
+				
+        -- Vorspann
+        ["Vorspann_br193"] = "Vorspann-Loks aktivieren?",
+        ["option_vorspann_br193_desc"] = "Die Vorspann-Loks (mit vorderem Panto gehoben) aktiviert werden?",
+        ["br193_vorspann_siemens_name_name"] = "Vorspann - BR 193 873 Siemens",
+
+        -- Fake
+        ["Fake_br193"] = "Fake-Loks aktivieren?",
+        ["option_fake_br193_desc"] = "Die Fake-Loks (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		
+        ["br193_fake_ms_siemens_name_name"] = 	"Fake - BR 193 873 Siemens Versuchslok",
+		
+		["br193_fake_ac_siemens_name_name"] = 	"Fake - BR 193 930 AC Siemens Versuchslok",
+		
+		["br192_fake_siemens_name_name"] = 		"Fake - BR 192 001 Siemens",
+
+	},
+}
+end
